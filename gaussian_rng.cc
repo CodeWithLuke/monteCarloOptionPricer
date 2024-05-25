9880d99e006310a03f25957e2b58261f34814371
CodeWithLuke <79409626+CodeWithLuke@users.noreply.github.com>
2024-05-25 16:56:12 -0400

    add ability to seed rng

--- gaussian_rng.cc
+++ gaussian_rng.cc
@@ -11,8 +11,16 @@ m_mean { mean }, m_stddev { stddev }, m_seed { seed }
 {}
 
 float GaussianRNG::getRandomNumber() {
-    static std::random_device rd;
-    static std::mt19937 eng(rd()); // seed the generator
+    static std::mt19937 eng;
+
+    if (m_seed.has_value()) {
+        eng = std::mt19937(m_seed.value());
+    }
+    else {
+        static std::random_device rd;
+        static std::mt19937 eng(rd()); // seed the generator
+    }
+    
     std::normal_distribution<> distr(m_mean, m_stddev);
     return distr(eng);
 }
