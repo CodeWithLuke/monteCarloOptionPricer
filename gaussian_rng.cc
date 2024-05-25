6731699094e2854e353b611955bbbd125cc0f291
CodeWithLuke <79409626+CodeWithLuke@users.noreply.github.com>
2024-05-24 21:58:06 -0400

    added ability to seed gaussian_rng

--- gaussian_rng.cc
+++ gaussian_rng.cc
@@ -13,6 +13,16 @@ m_mean { mean }, m_stddev { stddev }
     std::normal_distribution<> m_generator(mean, stddev);
 }
 
+GaussianRNG::GaussianRNG(float mean, float stddev, int seed) :
+m_mean { mean }, m_stddev { stddev }, m_seed { seed }
+{
+    // Create a random number generator
+    std::mt19937 m_gen(seed); // Mersenne Twister engine
+
+    // Create a normal (Gaussian) distribution
+    std::normal_distribution<> m_generator(mean, stddev);
+}
+
 float GaussianRNG::getRandomNumber() {
     return m_generator(m_gen);
 }
--- gaussian_rng.hpp
+++ gaussian_rng.hpp
@@ -2,17 +2,20 @@
 #define GAUSSIAN_RNG_HPP
 
 #include <random>
+#include <optional>
 
 class GaussianRNG
 {
 private:
     float m_mean;
     float m_stddev;
+    std::optional<int> m_seed;
     std::normal_distribution<> m_generator;
     std::mt19937 m_gen;
     
 public:
     GaussianRNG(float mean, float stddev);
+    GaussianRNG(float mean, float stddev, int seed);
     float getRandomNumber();
 };
 
