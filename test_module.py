49f5f93c46297145371c2c77b10696560ffc9b21
CodeWithLuke <79409626+CodeWithLuke@users.noreply.github.com>
2024-06-01 16:49:44 -0400

    add a python test

---
+++ test_module.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import sys
+
+import monte_carlo_option_pricer
+
+import datetime as dt
+
+if __name__ == '__main__':
+
+    print(monte_carlo_option_pricer.add(2, 3))
+    
+    print(monte_carlo_option_pricer.addDays(dt.datetime.now(), 2))
+
+    # print(monte_carlo_option_pricer.calc_monte_carlo_random_walk(2))
+
+
+
