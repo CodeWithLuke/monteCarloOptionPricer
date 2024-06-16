2c1c2689cdf09b191578ebea74384c5679218a27
CodeWithLuke <79409626+CodeWithLuke@users.noreply.github.com>
2024-06-16 11:41:44 -0400

    add test that should be false

--- src/unit_test.cc
+++ src/unit_test.cc
@@ -15,7 +15,7 @@ TEST(UnitTest, BasicAssertions)
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
     // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+    EXPECT_EQ(7 * 6, 43);
 
     auto test_date = date::sys_days{ date::year{2023}/5/21};
 
